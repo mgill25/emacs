150aca7646d608d391e2bce0952d9a50b1fca751
Manish Gill <mgill25@outlook.com>
2015-07-08 03:38:22 +0530

    Vim-like Split navigation, minor change with nodejs-repl-eval-region

--- evil.el
+++ evil.el
@@ -22,6 +22,12 @@
 (define-key evil-motion-state-map (kbd "C-e") nil)
 (define-key evil-visual-state-map (kbd "C-c") 'evil-exit-visual-state)
 
+;; navigation for splits
+(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+
 ;; Leader!
 ;; keyboard shortcuts
 (evil-leader/set-key
